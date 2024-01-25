35e27f6686e2c77143ce2b9f31b10765b1fc5b89
MatiasDeMori <matiasdemori@gmail.com>
2024-01-25 16:11:29 -0300

    Agrego mas productos

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import Contador from '../ItemCount/ItemCount';
 import { CarritoContext } from '../../context/CarritoContext';
 import { Link } from 'react-router-dom';
@@ -7,13 +6,20 @@ import './ItemDetail.scss';
 
 const ItemDetail = ({ id, nombre, img, stock, precio }) => {
   const [agregarCantidad, setAgregarCantidad] = useState(0);
-
-  const { agregarAlCarrito } = useContext(CarritoContext);
+  const [mostrarMensaje, setMostrarMensaje] = useState(false);
+  const { carrito, agregarAlCarrito } = useContext(CarritoContext);
 
   const manejadorCantidad = (cantidad) => {
-    setAgregarCantidad(cantidad);
-    const item = { id, nombre, precio };
-    agregarAlCarrito(item, cantidad);
+    const cantidadEnCarrito = carrito.find((item) => item.item.id === id)?.cantidad || 0;
+
+    if (cantidad + cantidadEnCarrito <= stock) {
+      setAgregarCantidad(cantidad);
+      const item = { id, nombre, precio };
+      agregarAlCarrito(item, cantidad);
+      setMostrarMensaje(false); 
+    } else {
+      setMostrarMensaje(true); 
+    }
   };
 
   return (
@@ -24,24 +30,28 @@ const ItemDetail = ({ id, nombre, img, stock, precio }) => {
           <h2>{nombre}</h2>
           <p>Precio: {precio}</p>
           <p>Stock: {stock}</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam
-            amet, quasi, repellendus dolore accusamus necessitatibus harum
-            maiores adipisci, aperiam deleniti facere vel maxime tempore
-            voluptatibus odit reiciendis corporis labore fuga!
-          </p>
         </div>
-        {stock > 0 && (
+        {stock > 0 ? (
           <>
+            {agregarCantidad === 0 ? (
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam
+                amet, quasi, repellendus dolore accusamus necessitatibus harum
+                maiores adipisci, aperiam deleniti facere vel maxime tempore
+                voluptatibus odit reiciendis corporis labore fuga!
+              </p>
+            ) : null}
             {agregarCantidad > 0 ? (
               <Link to="/cart">Terminar Compra</Link>
             ) : (
               <Contador inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />
             )}
           </>
+        ) : (
+          <p className="Sin-Stock">Por el momento no hay stock disponible de esta carta.</p>
         )}
-        {stock === 0 && (
-          <p className="Sin-Stock">Por el momento no hay stock disponible.</p>
+        {mostrarMensaje && (
+          <p className="mensaje-stock">Has alcanzado el número máximo de stock al intentar agregar al carrito.</p>
         )}
       </div>
     </div>
